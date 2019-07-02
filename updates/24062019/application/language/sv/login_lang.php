9f38d461d64b76de44315e39f544c0aaf6b2dbc5
primuszeus <adeloyedeji@gmail.com>
2019-07-02 10:31:10 +0100

    Conclusion of updates on 01072019

--- updates/24062019/application/language/sv/login_lang.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$lang["login_go"] = "";
-$lang["login_invalid_username_and_password"] = "";
-$lang["login_login"] = "";
-$lang["login_password"] = "";
-$lang["login_username"] = "";
--- updates/24062019/application/models/Item_Batches.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class ItemBatches extends CI_Model {
-
-        public function save(&$data) {
-            $this->db->insert('item', $data);
-            $data['id'] = $this->db->insert_id();
-            return TRUE;
-        }
-    }
